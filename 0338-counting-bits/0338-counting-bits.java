437f23a20054fb96c5ff2ac11cf5660381df844a
Chetan Kalakappa Ronad <chetankronad06@gmail.com>
2025-08-14 15:26:13 +0530

    Time: 3 ms (32.87%), Space: 46.7 MB (50.56%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr = new int[n+1];
+        for(int i=0;i<=n;i++){
+            arr[i]= hammingWeight(i);
+        }
+        return arr;
+        
+    }
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += (n & 1); 
+            n >>= 1;     
+        }
+        return count;
+    }
+}
