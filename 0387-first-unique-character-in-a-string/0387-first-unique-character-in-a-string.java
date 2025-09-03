a1504547847d9da47fc3b18ea3c8844bb25c7469
Chetan Kalakappa Ronad <chetankronad06@gmail.com>
2025-09-03 11:45:30 +0530

    Time: 47 ms (5.55%), Space: 45.8 MB (6.49%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -17,17 +17,38 @@ class Solution {
 
 
     // second soln
-    int[] freq = new int[26];
+    // int[] freq = new int[26];
 
-    for (char c : s.toCharArray()) {
-        freq[c - 'a']++;
-    }
-    for (int i = 0; i < s.length(); i++) {
-        if (freq[s.charAt(i) - 'a'] == 1) {
-            return i;
+    // for (char c : s.toCharArray()) {
+    //     freq[c - 'a']++;
+    // }
+    // for (int i = 0; i < s.length(); i++) {
+    //     if (freq[s.charAt(i) - 'a'] == 1) {
+    //         return i;
+    //     }
+    // }
+    // return -1;
+    // }
+
+
+    //third soln
+    HashMap<Character, Integer> map = new HashMap<>();
+    Queue<Integer> q = new ArrayDeque<>();
+    char[] arr = s.toCharArray();
+    for(int i = 0; i<arr.length;i++){
+        map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        q.offer(i);
+        while(!q.isEmpty() && map.get(arr[q.peek()])>1 ){
+            q.poll();
         }
+        
     }
-    return -1;
+    if(q.isEmpty()){
+            return -1;
+    }
+    else{
+        return q.peek();
     }
 
+    }
 }
