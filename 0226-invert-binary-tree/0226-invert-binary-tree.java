739b20057594aba132339e5b4e44e1e91fa5f210
Chetan Kalakappa Ronad <chetankronad06@gmail.com>
2026-05-06 17:02:08 +0530

    Time: 76 ms (0.09%), Space: 42.8 MB (64.55%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,17 +13,41 @@
  *     }
  * }
  */
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if(root == null){
+//             return root;
+//         }
+//         invertTree(root.left);
+//         invertTree(root.right);
+//         TreeNode curr = root.left;
+//         root.left = root.right;
+//         root.right = curr;
+//         return root;
+        
+//     }
+// }
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null){
-            return root;
+        if (root == null) return root; 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i =0 ; i<size;i++){
+                TreeNode node = queue.poll();
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+                TreeNode curr = node.left;
+                node.left = node.right;
+                node.right = curr;
+            }
         }
-        invertTree(root.left);
-        invertTree(root.right);
-        TreeNode curr = root.left;
-        root.left = root.right;
-        root.right = curr;
         return root;
-        
     }
 }
