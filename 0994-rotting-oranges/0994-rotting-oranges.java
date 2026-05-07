8e0aba5331f03586812cb26d70a922eb13a34d3e
Chetan Kalakappa Ronad <chetankronad06@gmail.com>
2026-05-07 23:13:28 +0530

    Time: 1 ms (100%), Space: 43.7 MB (94.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,43 +1,69 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if (grid.length == 0)
+        Queue<Pair> q=new ArrayDeque<>();
+        int r=grid.length;
+        int c=grid[0].length;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==2) q.add(new Pair(i,j));
+            }
+        }
+        if(r==1 && q.isEmpty()){
+            for(int j=0;j<c;j++){
+                if(grid[0][j]==1) return -1;
+            }
             return 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        int total = 0;
-        int count = 0;
-        Queue<int[]> rotten = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] != 0)
-                    total++;
-                if (grid[i][j] == 2) {
-                    rotten.add(new int[] { i, j });
-                }
-            }
         }
-        int[] dx = { 0, 0, 1, -1 };
-        int[] dy = { 1, -1, 0, 0 };
-        int days = 0;
-        while (!rotten.isEmpty()) {
-            int k = rotten.size();
-            count += k;
-            for (int i = 0; i < k; i++) {
+        int time=-1;
 
-                int[] pos = rotten.poll();
-                int x = pos[0], y = pos[1];
-                for (int d = 0; d < 4; d++) {
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
-                    if (nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1)
-                        continue;
-                    grid[nx][ny] = 2;
-                    rotten.add(new int[] { nx, ny });
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Pair curr=q.poll();
+                int x=curr.i;
+                int y=curr.j;
+
+                //up
+                if(x-1>=0 && grid[x-1][y]==1){
+                    q.add(new Pair(x-1,y));
+                    grid[x-1][y]=2;
+                }
+                //down
+                if(x+1<r && grid[x+1][y]==1){
+                    q.add(new Pair(x+1,y));
+                    grid[x+1][y]=2;
+                }
+                //right
+                if(y+1<c && grid[x][y+1]==1){
+                    q.add(new Pair(x,y+1));
+                    grid[x][y+1]=2;
+                }
+                //left
+                if(y-1>=0 && grid[x][y-1]==1){
+                    q.add(new Pair(x,y-1));
+                    grid[x][y-1]=2;
                 }
             }
-            if (!rotten.isEmpty())
-                days++;
+            time++;
         }
-        return total == count ? days : -1;
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1) return -1;
+            }
+        }
+
+        return time;
+    }
+
+
+}
+
+class Pair{
+    int i;
+    int j;
+    public Pair(int i,int j){
+        this.i=i;
+        this.j=j;
     }
 }
