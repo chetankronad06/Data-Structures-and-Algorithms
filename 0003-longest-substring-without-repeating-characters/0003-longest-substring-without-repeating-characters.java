fcb79afb1c7cc7750a77678f9b87d36d24f1de30
Chetan Kalakappa Ronad <chetankronad06@gmail.com>
2026-09-04 23:06:50 +0530

    Time: 12 ms (57.59%), Space: 47.9 MB (25.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,19 +1,56 @@
-class Solution {
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         int maxLength = 0;
+//         int[] charIndex = new int[128];
+//         Arrays.fill(charIndex, -1);
+//         int left = 0;
+        
+//         for (int right = 0; right < n; right++) {
+//             if (charIndex[s.charAt(right)] >= left) {
+//                 left = charIndex[s.charAt(right)] + 1;
+//             }
+//             charIndex[s.charAt(right)] = right;
+//             maxLength = Math.max(maxLength, right - left + 1);
+//         }
+        
+//         return maxLength;
+//     }
+// }
+
+// class Solution{
+//     public int lengthOfLongestSubstring(String s) {
+//         int[] hash = new int[256];
+//         int maxLen = 0;
+//         for(int i=0;i<s.length();i++){
+//             for(int j=i;j<s.length();j++){
+//                 if(hash[s.charAt(j)]==1) break;
+//                 int len = j-i+1;
+//                 maxLen = Math.max(maxLen,len);
+//                 hash[s.charAt(j)]=1;
+//             }
+//         }
+//         return maxLen;
+//     }
+
+// }
+
+
+class Solution{
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int[] hash = new int[256];
-        Arrays.fill(hash, -1);
-
-        int l = 0, maxLen = 0;
-
-        for (int r = 0; r < n; r++) {
-            int c = s.charAt(r);
-            if (hash[c] >= l) {
-                l = hash[c] + 1;
+        int left = 0;
+        int right = 0;
+        int minLength = 0;
+        int hash[] = new int[256];
+        Arrays.fill(hash,-1);
+        while(right < s.length()){
+            if(hash[s.charAt(right)]!=-1 && hash[s.charAt(right)]>=left){
+                left = hash[s.charAt(right)]+1;
             }
-            maxLen = Math.max(maxLen, r - l + 1);
-            hash[c] = r;
+            minLength = Math.max(minLength,right-left+1);
+            hash[s.charAt(right)]= right;
+            right++;
         }
-        return maxLen;
+        return minLength;
     }
 }
