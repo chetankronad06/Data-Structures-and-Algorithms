a57080191a05f9ee3415b0a8295dfbaf3d848892
Chetan Kalakappa Ronad <chetankronad06@gmail.com>
2026-09-14 12:08:31 +0530

    Time: 0 ms (100%), Space: 46.8 MB (13.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,17 +9,39 @@
  *     }
  * }
  */
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         ListNode temp = head ;
+//         HashMap<ListNode,Integer> map = new HashMap<>();
+//         while(temp!=null){
+//             if(map.containsKey(temp)){
+//                 return temp;
+//             }
+//             map.put(temp,1);
+//             temp = temp.next;
+//         }
+//         return null;
+//     }
+// }
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode temp = head ;
-        HashMap<ListNode,Integer> map = new HashMap<>();
-        while(temp!=null){
-            if(map.containsKey(temp)){
-                return temp;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
-            map.put(temp,1);
-            temp = temp.next;
+
         }
+
         return null;
+
     }
 }
